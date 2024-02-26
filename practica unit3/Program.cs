9b45141576ce619f5a0ef5d2174aeb3e646c2b63
Israel Zacarias <Israel Zacarias@DESKTOP-56CI4RG>
2024-02-26 12:02:15 -0600

    Agregar archivos de proyecto.

---
+++ practica unit3/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace practica_unit3 
+{ 
+ internal class program
+ {
+        public static void Main(string args)
+        {
+            area_circulo uno = new area_circulo();
+            uno.circulo(2.8);
+            Console.WriteLine(uno.Formula);
+        }
+        
+        
+
+
+
+ }
+}
+
+
---
+++ practica unit3/area circulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_unit3
+{
+    internal class area_circulo
+    {
+        private double radio;
+
+        public double Radio { get => radio; set => radio = value; }
+
+        public area_circulo() 
+        { 
+           radio = 0;
+        }
+        public void circulo(double radio_)
+        {
+            radio = radio_;
+        }
+        public double Formula (double _area) 
+        { 
+            return _area= 3.1416*(radio *radio);
+        }
+
+    }
+}
